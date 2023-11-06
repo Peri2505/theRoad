ca4893a828c688d01154cefc58a0585d3e012111
Peri2505 <moldobaevaperi@yahoo.com>
2023-11-06 13:04:58 -0600

    The Road

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.querySelector(".close-navbar-icon").addEventListener("click", () => {
 
 const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 let i = 0;
-
+//
 Array.from(document.querySelectorAll(".nav-link")).forEach((item) => {
   item.style.cssText = `background-color:${colors[i++]}`;
 });
